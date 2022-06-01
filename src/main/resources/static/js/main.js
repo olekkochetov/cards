72e4321e450204f3802ccc8494b42ae662a6de52
alexkochetov <kchetovalex@yahoo.com>
2022-06-01 21:29:30 +0200

    add styles and scripts

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,3 +6,13 @@ span.classList.add("year");
 const mainFooter = document.querySelector("#site_footer");
 span.innerHTML = year;
 mainFooter.appendChild(span);
+
+let elements = document.querySelectorAll(".center");
+//let hidden;
+elements.forEach((element, index) => {
+	element.addEventListener("click", function(event){
+		let hidden = document.querySelector(".hidden");
+		element.classList.add("hidden");
+		hidden.classList.remove("hidden");
+	});	
+});
