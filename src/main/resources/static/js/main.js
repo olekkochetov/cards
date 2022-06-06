6cfcf61736695ae4459e3c5a177b70b0a20b3c4c
alexkochetov <kchetovalex@yahoo.com>
2022-06-06 22:05:54 +0200

    change basic styles

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,4 @@
+/** Date */
 let date = new Date();
 let year = date.getFullYear();
 let span = document.createElement("span");
@@ -6,6 +7,11 @@ span.classList.add("year");
 const mainFooter = document.querySelector("#site_footer");
 span.innerHTML = year;
 mainFooter.appendChild(span);
+
+
+/**
+ * Flip Card
+ */
 
 let elements = document.querySelectorAll(".center");
 //let hidden;
@@ -16,3 +22,32 @@ elements.forEach((element, index) => {
 		hidden.classList.remove("hidden");
 	});	
 });
+
+/**Caret */
+const caretElements = document.querySelectorAll(".caret");
+
+/**Description */
+
+caretElements.forEach((element) => {
+	element.addEventListener("click", function(){
+		let theme = element.parentNode.parentNode.parentNode;
+		console.log(theme);
+		const description = theme.querySelector(".description");
+		description.classList.toggle("hidden");
+	});
+});
+
+/**Delete */
+
+const deleteButtons = document.querySelectorAll(".delete");
+
+deleteButtons.forEach((btn) => {
+	btn.addEventListener("click", (event) => {
+		if(!confirm("delete ?")) {
+			event.preventDefault();
+		}
+	});
+});
+
+
+
