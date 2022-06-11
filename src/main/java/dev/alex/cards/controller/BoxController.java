833880a4561aa4b688db39e5a93b66a9237b2ec3
alexkochetov <kchetovalex@yahoo.com>
2022-06-11 18:25:16 +0200

    add custom query for box and theme

--- src/main/java/dev/alex/cards/controller/BoxController.java
+++ src/main/java/dev/alex/cards/controller/BoxController.java
@@ -24,4 +24,9 @@ public class BoxController {
         }
         return "redirect:/";
     }
+    
+    @GetMapping("")
+    public String showAllCardsInBox() {
+    	return "";
+    }
 }
--- src/main/java/dev/alex/cards/repository/BoxRepository.java
+++ src/main/java/dev/alex/cards/repository/BoxRepository.java
@@ -1,11 +1,15 @@
 package dev.alex.cards.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import dev.alex.cards.model.Box;
+import dev.alex.cards.model.Card;
 
 @Repository
 public interface BoxRepository extends JpaRepository<Box, Long> {
-
+	
 }
