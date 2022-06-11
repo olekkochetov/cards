b3e6aead06d774d09aaa526abc6a41a9f420d684
alexkochetov <kchetovalex@yahoo.com>
2022-06-11 22:32:50 +0200

    add query for box

--- src/main/java/dev/alex/cards/repository/BoxRepository.java
+++ src/main/java/dev/alex/cards/repository/BoxRepository.java
@@ -11,5 +11,6 @@ import dev.alex.cards.model.Card;
 
 @Repository
 public interface BoxRepository extends JpaRepository<Box, Long> {
-	
+	@Query("Select b, c FROM Box b INNER JOIN b.cards c WHERE b.id = ?1")
+	List<Box> getCardsByBoxId(int boxId);
 }
